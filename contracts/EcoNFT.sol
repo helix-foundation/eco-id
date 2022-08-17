d57171d4b343392ef79e9d07002c480a1c8901d8
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2022-08-17 12:02:25 -0400

    updating nft contract to not use ownable and allow multiple minters

--- contracts/EcoNFT.sol
+++ contracts/EcoNFT.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 /**
@@ -11,7 +10,7 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
  * mint an EcoNFT. The issued NFT is soulbound to the address it was issue to, and cannot be transfered. Only one EcoNFT can ever be
  * minted per social account.
  */
-contract EcoNFT is ERC721("EcoNFT", "EcoNFT"), Ownable {
+contract EcoNFT is ERC721("EcoNFT", "EcoNFT") {
     /**
      * Use for signarture recovery and verification on minting of EcoNFT
      */
@@ -36,18 +35,20 @@ contract EcoNFT is ERC721("EcoNFT", "EcoNFT"), Ownable {
      * Mints an EcoNFT if the discord and twitter IDs have not been claimed yet, and only when the owener of this EcoNFT contract
      * has signed off on the minting
      *
-     * Parameters:
-     *  - discordID/twitterID the social ids of the user
-     *  - signature is signature that we are validating comes from the owner of this contract, ie the minter account has signed off
+     * @param socialID the social ids of the user
+     * @param recipientAddress the address of the recipient of the newly minted nft
+     * @param minterAddress the address of the minter for the nft, that has verified the socialID
+     * @param signature signature that we are validating comes from the minterAddress
      */
     function mintEcoNFT(
         string calldata socialID,
         address recipientAddress,
+        address minterAddress,
         bytes calldata signature
     ) external returns (uint256) {
         require(hasNotBeenMinted(socialID), "social has minted token");
         require(
-            _verifyMint(socialID, recipientAddress, signature),
+            _verifyMint(socialID, recipientAddress, minterAddress, signature),
             "signature did not match"
         );
 
@@ -64,6 +65,8 @@ contract EcoNFT is ERC721("EcoNFT", "EcoNFT"), Ownable {
     /**
      * Returns the NTF ID for a given social id that we have linked the user too. The function takes the
      * hash of the social id and returns it as a token id uint256
+     *
+     * @param socialID the social ids of the user
      */
     function socialToNFTID(string calldata socialID)
         internal
@@ -76,8 +79,7 @@ contract EcoNFT is ERC721("EcoNFT", "EcoNFT"), Ownable {
     /**
      * Checks that the social id has not had an EcoNFT already minted
      *
-     * Parameters:
-     *  - discordID/twitterID the social ids of the user
+     * @param socialID the social ids of the user
      */
     function hasNotBeenMinted(string calldata socialID)
         internal
@@ -90,26 +92,29 @@ contract EcoNFT is ERC721("EcoNFT", "EcoNFT"), Ownable {
     /**
      * Verifies the signature supplied belongs to the owner address.
      *
-     * Parameters:
-     *  - socialID the social ids of the user
-     *  - recipientAddress the address of the user
-     *  - signature is signature that we are validating comes from the owner of this contract, ie the minter account has signed off
+     * @param socialID the social ids of the user
+     * @param recipientAddress the address of the user that gets the nft
+     * @param minterAddress  the address of the minter for the nft, that has verified the socialID
+     * @param signature signature that we are validating comes from the minterAddress
      *
-     * Returns:
-     *  - true if the signature is valid, false otherwise
+     * @return true if the signature is valid, false otherwise
      */
     function _verifyMint(
         string calldata socialID,
         address recipientAddress,
+        address minterAddress,
         bytes calldata signature
-    ) internal view returns (bool) {
+    ) internal pure returns (bool) {
         bytes32 hash = getNftHash(socialID, recipientAddress);
-        return hash.recover(signature) == owner();
+        return hash.recover(signature) == minterAddress;
     }
 
     /**
      * Hashes the input parameters and hashes using keccak256,
      * attaches eth_sign_message for a validator verification
+     *
+     * @param socialID the social ids of the user
+     * @param recipientAddress the address of the user that gets the nft
      */
     function getNftHash(string calldata socialID, address recipientAddress)
         private
